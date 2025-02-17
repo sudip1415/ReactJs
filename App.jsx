e0336f0b007b4b7d6c76689d5779d77fcc665603
Sudip <152992299+sudip1415@users.noreply.github.com>
2025-02-17 17:33:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tasks from './Components/Tasks'
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-500 p-4">
+            <Tasks />
+        </div>
+    );
+}
+
+export default App
